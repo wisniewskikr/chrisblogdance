bcfa2900f77f9e612fb4954398e0b85c3e4914ef
Wisniewski <wisniewk@emea.roche.com>
2019-08-27 14:52:26 +0200

    Clean project

--- src/main/java/pl/kwi/springboot/controllers/InitController.java
+++ src/main/java/pl/kwi/springboot/controllers/InitController.java
@@ -4,12 +4,12 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-@RequestMapping(value="/home")
-public class HomeController {
+@RequestMapping(value="/")
+public class InitController {
 
 	@RequestMapping
-	public String displayPage() {
-		return "home";
+	public String index() {
+		return "redirect:home";
 	}
-
+	
 }
