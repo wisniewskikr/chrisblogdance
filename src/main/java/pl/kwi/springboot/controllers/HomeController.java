6cc44b4330121f6e1a80cef239e8203212dcfce9
Wisniewski <wisniewk@emea.roche.com>
2019-08-28 18:14:58 +0200

    Add repository to controller

--- src/main/java/pl/kwi/springboot/controllers/HomeController.java
+++ src/main/java/pl/kwi/springboot/controllers/HomeController.java
@@ -1,14 +1,27 @@
 package pl.kwi.springboot.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import pl.kwi.springboot.db.entities.DanceTypeEntity;
+import pl.kwi.springboot.db.repositories.DanceTypeRepository;
 
 @Controller
 @RequestMapping(value="/home")
 public class HomeController {
 
+	@Autowired
+	private DanceTypeRepository danceTypeRepository;	
+	
 	@RequestMapping
 	public String displayPage() {
+		
+		Iterable<DanceTypeEntity> danceTypes = danceTypeRepository.findAll();
+		for (DanceTypeEntity danceTypeEntity : danceTypes) {
+			System.out.println(danceTypeEntity.getName());
+		}
+		
 		return "home";
 	}
 
